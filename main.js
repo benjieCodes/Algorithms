49ffb2d2deb564c72c37cce55723381e15cc605a
Benjie Songsong <benjiesongsong@gmail.com>
2016-10-19 12:51:30 -0400

    working on Q1

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 console.log('hello world');
+
 // Answer to Question one:
+/*- Steps to find the answer
+1. Create a function = getMaxProfit()
+2. Create an array in the function for the stocks = stockPricesYesterday = []
+3. Find the highest/lowest value of the stocks
+4. Subtract those values together (highest - lowest)
+5. Return that answer to the function
+*/
+
+function getMaxProfit (stockPricesYesterday) {
+  // need a command that finds highest value in an array
+}
