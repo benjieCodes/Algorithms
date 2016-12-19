9cf7ac8ec9a70b963ca0d66f19da2682ca3fb2df
benjiecodes <benjiecodes@gmail.com>
2016-12-19 10:55:50 -0500

    added an array of functions

--- main.js
+++ main.js
@@ -106,3 +106,17 @@ function rentalCarCost(d) {
     return total;
   }
 }
+
+// Answer for Question 8, creating array commands
+Array.prototype.square = function () { return this.map(function (num) {return Math.pow(num, 2)})}
+Array.prototype.cube = function() { return this.map (function (num) {return Math.pow(num, 3)})}
+Array.prototype.average = function () {
+  if (this.length === 0) return NaN;
+  return this.reduce (function (x, y) { return x + y / this.length });
+}
+Array.prototype.sum = function () {
+  if (this.length === 0) return NaN;
+  return this.reduce (function (x, y) { return x + y});
+}
+Array.prototype.even = function () { return this.filter (function (x) {return x % 2 === 0})}
+Array.prototype.odd = function () { return this.filter (function (x) {return x % 2 === 1 })}
